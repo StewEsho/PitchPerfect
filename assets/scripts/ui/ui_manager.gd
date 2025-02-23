4e163bd8bd04b7640b11caa24c0a6dfb3e79291a
Stew Esho <stewardesho@gmail.com>
2025-02-23 00:42:13 -0800

    added score to lose-screen

--- assets/scripts/ui/ui_manager.gd
+++ assets/scripts/ui/ui_manager.gd
@@ -1,8 +1,20 @@
 extends Control
 
+var ScoreRects: Array[TextureRect] = []
+
+func load_rect(num: int) -> void:
+	var tex = load("res://assets/art/ui/score/score_%d.png" % num)
+	var rect := TextureRect.new()
+	rect.texture = tex
+	rect.visible = false
+	rect.z_index = 10
+	add_child(rect)
+	ScoreRects.append(rect)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in range(9):
+		load_rect(i)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +25,7 @@ func _on_umpire_game_over(score: int) -> void:
 	$BatterUI/Animator.play("Close")
 	$LoseScreen.visible = true
 	$LoseScreen/Audio.play()
+	ScoreRects[score].visible = true
 
 func reset() -> void:
 	print("PitchingHud::reset()")
@@ -21,6 +34,8 @@ func reset() -> void:
 	$BattersBox.visible = true
 	$LoseScreen.visible = false
 	$WinScreen.visible = false
+	for i in range(9):
+		ScoreRects[i].visible = false
 
 func _on_umpire_you_win() -> void:
 	$AnimationPlayer.play("hide_hud")
