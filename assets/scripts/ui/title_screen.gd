179e4485138a130671931a2abf6c104b6427350b
Stew Esho <stewardesho@gmail.com>
2025-02-23 01:44:01 -0800

    fix loading issue

--- assets/scripts/ui/title_screen.gd
+++ assets/scripts/ui/title_screen.gd
@@ -1,8 +1,19 @@
 extends Control
 
-var main_scene = preload("res://assets/scenes/main.tscn").instantiate()
+var game_start: bool = false
+
+func _ready() -> void:
+	$Game/background.visible = false
+	$Game/PitchingHud.visible = false
+	$Game.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _input(event: InputEvent) -> void:
-	if not event.is_echo() and event.is_action_pressed("pitch"):
-		get_tree().root.add_child(main_scene)
-		get_node("/root/TitleScreen").free()
+	if !game_start and !event.is_echo() and event.is_action_pressed("pitch"):
+		game_start = true
+		$TitleScreenBg.visible = false
+		$TitleScreenRender.visible = false
+		$TitleScreenContinue.visible = false
+		$Game.process_mode = Node.PROCESS_MODE_INHERIT
+		$Game/PitchingHud.visible = true
+		$Game/background.visible = true
+		
